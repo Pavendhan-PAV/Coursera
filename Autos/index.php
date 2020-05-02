98b1576a7e9859c16f0881e84dafc6ae1be7af8c
Pavendhan-PAV <53622544+Pavendhan-PAV@users.noreply.github.com>
2020-05-02 18:00:21 +0530

    Add files via upload

---
+++ Autos/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PAVENDHAN N</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Autos Database</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
+
